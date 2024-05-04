9093a8b0fb4baa9d9de09b88c3689729b2cc773e
xifeng jin <xifeng.jin88@gmail.com>
2024-05-04 17:19:37 -0500

    pricing page done

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,11 +1,12 @@
 import { prisma } from "@/lib/server";
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { notFound, redirect } from "next/navigation";
-import { LayoutDashboard } from "lucide-react";
+import { LayoutDashboard, ListChecks, DollarSign } from "lucide-react";
 import TitleForm from "./_components/title-form";
 import DescriptionForm from "./_components/description-form";
 import ImageForm from "./_components/image-form";
 import CategoryFrom from "./_components/category-form";
+import PriceForm from "./_components/price-form";
 
 async function getCourse({ courseId }: { courseId: string }) {
   const { userId } = auth();
@@ -70,7 +71,10 @@ export default async function CoursePage({
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
         <div>
           <div className="flex items-center">
-            <LayoutDashboard className="w-6 h-6 mr-2 bg-green-50" />
+            <div className="bg-green-100 rounded-full w-8 h-8 flex items-center justify-center mr-2">
+              <LayoutDashboard className="w-6 h-6" />
+            </div>
+
             <h1 className="text-xl">Customize your course</h1>
           </div>
           <TitleForm courseId={params.courseId} course={course} />
@@ -81,6 +85,28 @@ export default async function CoursePage({
             courseId={params.courseId}
             course={course}
           />
+        </div>
+        <div>
+          <div className="flex items-center">
+            <div className="bg-green-100 rounded-full w-8 h-8 flex items-center justify-center mr-2">
+              <ListChecks className="w-6 h-6" />
+            </div>
+
+            <h1 className="text-xl">Course Chapter</h1>
+          </div>
+          <div>Todo</div>
+          <div>
+            <div className="flex items-center">
+              <div className="bg-green-100 rounded-full w-8 h-8 flex items-center justify-center mr-2">
+                <DollarSign className="w-6 h-6" />
+              </div>
+
+              <h1 className="text-xl">Sell your course</h1>
+            </div>
+            <div>
+              <PriceForm course={course} courseId={params.courseId} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
